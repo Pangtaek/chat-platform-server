8e3a89723746d1b0b5aff17dccf6e84fc202e720
Pangtaek <zombie3632@gmail.com>
2025-02-08 18:45:48 +0900

    style: 불필요 Repository 어노테이션 제거

--- src/main/java/com/pangtaek/chatplatform/domain/repository/UserRespository.java
+++ src/main/java/com/pangtaek/chatplatform/domain/repository/UserRespository.java
@@ -3,11 +3,9 @@ package com.pangtaek.chatplatform.domain.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.pangtaek.chatplatform.domain.repository.entity.User;
 
-@Repository
 public interface UserRespository extends JpaRepository<User, Long> {
     
     Optional<User> findByName(String name);
