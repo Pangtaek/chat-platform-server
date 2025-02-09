f758935f92ec6e20c9dd96bb52ee693f925c87dc
Pangtaek <zombie3632@gmail.com>
2025-02-09 21:09:23 +0900

    fix: cors 설정 요청 메서드 오류 해결 #4

--- src/main/java/com/pangtaek/chatplatform/config/WebConfig.java
+++ src/main/java/com/pangtaek/chatplatform/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로 허용
                 .allowedOriginPatterns("http://localhost:*")
-                .allowedMethods("GET, HOST, PUT, DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true) // 자격 증명 허용
                 .maxAge(3600); // preflight 요청 캐시 시간
